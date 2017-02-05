7ac9ebe7cb30a0151153688bccebbb0e9f4282bd
Yang Liu <byliuyang11@gmail.com>
2017-02-05 10:07:43 -0500

    Link arrow to class diagram

--- src/app/diagrams/show/show.component.ts
+++ src/app/diagrams/show/show.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ElementRef} from '@angular/core';
 import {ClassInfo} from "../shared/class-info.model";
+import {ReferenceInfo} from "../shared/reference-info.model";
 
 @Component({
   selector: 'app-show',
@@ -9,7 +10,8 @@ import {ClassInfo} from "../shared/class-info.model";
 export class ShowComponent implements OnInit {
 
   classInfos: ClassInfo[];
-  constructor() { }
+  refInfos: ReferenceInfo[];
+  constructor(private el: ElementRef) { }
 
   ngOnInit() {
     this.classInfos = [
@@ -32,6 +34,58 @@ export class ShowComponent implements OnInit {
         "repo_uuid": "7a534e80-eb80-11e6-9ae0-4d981379eb9f"
       }
     ];
+
+    this.refInfos = [
+      {
+        "uuid": "7a940100-eb80-11e6-9ae0-4d981379eb9f",
+        "createdAt": "2017-02-05T08:52:52.624Z",
+        "updatedAt": "2017-02-05T08:52:52.624Z",
+        "class_uuid": "7a7d1da0-eb80-11e6-9ae0-4d981379eb9f",
+        "method_uuid": "7a8f4610-eb80-11e6-9ae0-4d981379eb9f"
+      }
+    ];
   }
 
+  getMethodAndClassEl(refInfo: ReferenceInfo) {
+    let classId = refInfo.class_uuid,
+        methodId = refInfo.method_uuid;
+    let methodEl = this.el.nativeElement.querySelector(`[data-method-id="${methodId}"]`),
+        classEl = this.el.nativeElement.querySelector(`[data-class-id="${classId}"]`);
+    return {methodEl, classEl};
+  }
+
+  isToLeft(els) {
+    return els.methodEl.offsetLeft + els.methodEl.offsetWidth < els.classEl.offsetLeft;
+  }
+
+  getStartX(refInfo: ReferenceInfo) {
+    let els = this.getMethodAndClassEl(refInfo);
+    let clientRect = els.methodEl.getBoundingClientRect();
+
+    return this.isToLeft(els) ?
+        clientRect.right + 5 :
+        clientRect.left - 5;
+  }
+
+  getStartY(refInfo: ReferenceInfo) {
+    let els = this.getMethodAndClassEl(refInfo);
+    let clientRect = els.methodEl.getBoundingClientRect();
+
+    return clientRect.bottom + 17;
+  }
+
+  getEndX(refInfo: ReferenceInfo) {
+    let els = this.getMethodAndClassEl(refInfo);
+    let clientRect = els.classEl.getBoundingClientRect();
+
+    return this.isToLeft(els) ?
+        (clientRect.left - 11) :
+        (clientRect.right + 11);
+  }
+  getEndY(refInfo: ReferenceInfo) {
+    let els = this.getMethodAndClassEl(refInfo);
+    let clientRect = els.classEl.getBoundingClientRect();
+
+    return (clientRect.top + clientRect.bottom) / 2.0;
+  }
 }
